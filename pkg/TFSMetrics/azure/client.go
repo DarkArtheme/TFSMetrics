31e46a01505700ce76786527dddb9b60e7afa2ef
Alexey <aleksey.vologdin@ya.ru>
2021-09-24 21:31:57 +0300

    work on function ChangedRows

--- pkg/TFSMetrics/azure/client.go
+++ pkg/TFSMetrics/azure/client.go
@@ -1,10 +1,8 @@
 package azure
 
 import (
-	"fmt"
 	"io"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
@@ -127,121 +125,37 @@ func (a *Azure) GetChangesetChanges(id *int, project string) (*ChangeSet, error)
 	return commit, nil
 }
 
-func (a *Azure) ChangedRows(nameProject string, currentFileUrl string, PreviousFileUrl string) (int, int, error) {
+func (a *Azure) ChangedRows(currentFileUrl string) (int, int, error) {
 	//1 GET FILES
-	_, previousVersion := a.GetItemVersions(nameProject) //получить версии изменений
-	if previousVersion == 0 {                            //посчитать изменения только в текущей версии
-		return a.getAddedRowsOneFile(currentFileUrl)
-	}
-
+	//get current version
 	item, err := a.TfvcClient.GetItemContent(a.Config.Context, tfvc.GetItemContentArgs{Path: &currentFileUrl})
 	if err != nil {
 		return 0, 0, err
 	}
-
 	b1, err := io.ReadAll(item)
 	if err != nil { //Read All File in array byte
 		return 0, 0, err
 	}
-
-	ver := fmt.Sprint(previousVersion) // заменить на код для разных версий
+	//get previous version
 	item1, err := a.TfvcClient.GetItemContent(a.Config.Context, tfvc.GetItemContentArgs{Path: &currentFileUrl,
-		VersionDescriptor: &git.TfvcVersionDescriptor{Version: &ver}})
+		VersionDescriptor: &git.TfvcVersionDescriptor{VersionOption: &git.TfvcVersionOptionValues.Previous}})
 	if err != nil {
-		return 0, 0, err
+		return a.getAddedRowsOneFile(&b1) //если нет прошлой версии считаем кол-во строк в текущем файле
 	}
 	b2, err := io.ReadAll(item1)
 	if err != nil {
 		return 0, 0, err
 	}
 
-	//2 конвертируем полученные данные в map
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-	var currentStringsMap, copyCurrentStringsMap, previousStringsMap, copyPreviousStringsMap map[int]string
-	go transformArrByteToMap(&b1, &wg, &currentStringsMap, &copyCurrentStringsMap)
-	go transformArrByteToMap(&b2, &wg, &previousStringsMap, &copyPreviousStringsMap)
-	wg.Wait()
-
-	//3 счатаем добавленные строки
-	//counters
-	addedRows := 0
-	deletedRows := 0
-
-	wg.Add(2)
-	go countAddedRows(copyCurrentStringsMap, copyPreviousStringsMap, &addedRows, &wg) //count added strings
-	go countDeleteRows(currentStringsMap, previousStringsMap, &deletedRows, &wg)      //count delete strings
-	wg.Wait()
-
-	return addedRows, deletedRows, err
+	//считаем добаленные и удаленные строки
+	addedRows, deletedRows := Diff(string(b2), string(b1))
+	return addedRows, deletedRows, nil
 }
 
-func (a *Azure) getAddedRowsOneFile(currentFileUrl string) (int, int, error) {
-	item, err := a.TfvcClient.GetItemContent(a.Config.Context, tfvc.GetItemContentArgs{Path: &currentFileUrl})
-	if err != nil {
-		return 0, 0, err
-	}
-
-	b1, err := io.ReadAll(item)
-	if err != nil { //Read All File in array byte
-		return 0, 0, err
-	}
-
-	transformString := string(b1)                               //transform array byte in string
+func (a *Azure) getAddedRowsOneFile(fileInBytes *[]byte) (int, int, error) {
+	transformString := string(*fileInBytes)                     //transform array byte in string
 	arrTransformStrings := strings.Split(transformString, "\n") //split string
 	return len(arrTransformStrings), 0, nil
-}
-
-func transformArrByteToMap(arr *[]byte, wg *sync.WaitGroup, transformMap, transformMapCopy *map[int]string) {
-	defer wg.Done()
-	transformString := string(*arr)                             //transform array byte in string
-	arrTransformStrings := strings.Split(transformString, "\n") //split string
-	//create maps
-	*transformMap = make(map[int]string, len(arrTransformStrings))
-	*transformMapCopy = make(map[int]string, len(arrTransformStrings))
-	//add strings in map
-	for k, v := range arrTransformStrings {
-		(*transformMap)[k] = v
-		(*transformMapCopy)[k] = v
-	}
-}
-
-func countAddedRows(currentStringsMap, previousStringsMap map[int]string, addedRows *int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	i := 0 //for chek end currentStrings
-	for _, v1 := range currentStringsMap {
-		i = 0
-		for k, v2 := range previousStringsMap {
-			if v1 == v2 {
-				delete(previousStringsMap, k)
-				i--
-				break
-			}
-			i++
-		}
-		if i == len(previousStringsMap) {
-			(*addedRows)++
-		}
-	}
-}
-
-func countDeleteRows(currentStringsMap, previousStringsMap map[int]string, deletedRows *int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	i := 0 //for chek end currentStrings
-	for _, v1 := range previousStringsMap {
-		i = 0
-		for k, v2 := range currentStringsMap {
-			if v1 == v2 {
-				delete(currentStringsMap, k)
-				i--
-				break
-			}
-			i++
-		}
-		if i == len(currentStringsMap) {
-			(*deletedRows)++
-		}
-	}
 }
 
 // заглушка чтобы избавиться от ошибки нереализованного интерфейса
@@ -252,4 +166,167 @@ func (a *Azure) GetItemVersions(ChangesUrl string) (int, int) {
 	}
 
 	return *(changesets)[0], 0
+}
+
+//diff
+type Chunk struct {
+	Added   []string
+	Deleted []string
+	Equal   []string
+}
+
+func (c *Chunk) empty() bool {
+	return len(c.Added) == 0 && len(c.Deleted) == 0 && len(c.Equal) == 0
+}
+
+// Diff returns a string containing a line-by-line unified diff of the linewise
+// changes required to make A into B.  Each line is prefixed with '+', '-', or
+// ' ' to indicate if it should be added, removed, or is correct respectively.
+func Diff(A, B string) (int, int) {
+	aLines := strings.Split(A, "\n")
+	bLines := strings.Split(B, "\n")
+	return Render(DiffChunks(aLines, bLines))
+}
+
+// Render renders the slice of chunks into a representation that prefixes
+// the lines with '+', '-', or ' ' depending on whether the line was added,
+// removed, or equal (respectively).
+func Render(chunks []Chunk) (int, int) {
+	addedRows := 0
+	deletedRows := 0
+
+	for _, v := range chunks {
+		if len(v.Added) == 1 {
+			addedRows++
+		} else if len(v.Deleted) == 1 {
+			deletedRows++
+		}
+	}
+	return addedRows, deletedRows
+}
+
+// DiffChunks uses an O(D(N+M)) shortest-edit-script algorithm
+// to compute the edits required from A to B and returns the
+// edit chunks.
+func DiffChunks(a, b []string) []Chunk {
+	// algorithm: http://www.xmailserver.org/diff2.pdf
+
+	// We'll need these quantities a lot.
+	alen, blen := len(a), len(b) // M, N
+
+	// At most, it will require len(a) deletions and len(b) additions
+	// to transform a into b.
+	maxPath := alen + blen // MAX
+	if maxPath == 0 {
+		// degenerate case: two empty lists are the same
+		return nil
+	}
+
+	// Store the endpoint of the path for diagonals.
+	// We store only the a index, because the b index on any diagonal
+	// (which we know during the loop below) is aidx-diag.
+	// endpoint[maxPath] represents the 0 diagonal.
+	//
+	// Stated differently:
+	// endpoint[d] contains the aidx of a furthest reaching path in diagonal d
+	endpoint := make([]int, 2*maxPath+1) // V
+
+	saved := make([][]int, 0, 8) // Vs
+	save := func() {
+		dup := make([]int, len(endpoint))
+		copy(dup, endpoint)
+		saved = append(saved, dup)
+	}
+
+	var editDistance int // D
+dLoop:
+	for editDistance = 0; editDistance <= maxPath; editDistance++ {
+		// The 0 diag(onal) represents equality of a and b.  Each diagonal to
+		// the left is numbered one lower, to the right is one higher, from
+		// -alen to +blen.  Negative diagonals favor differences from a,
+		// positive diagonals favor differences from b.  The edit distance to a
+		// diagonal d cannot be shorter than d itself.
+		//
+		// The iterations of this loop cover either odds or evens, but not both,
+		// If odd indices are inputs, even indices are outputs and vice versa.
+		for diag := -editDistance; diag <= editDistance; diag += 2 { // k
+			var aidx int // x
+			switch {
+			case diag == -editDistance:
+				// This is a new diagonal; copy from previous iter
+				aidx = endpoint[maxPath-editDistance+1] + 0
+			case diag == editDistance:
+				// This is a new diagonal; copy from previous iter
+				aidx = endpoint[maxPath+editDistance-1] + 1
+			case endpoint[maxPath+diag+1] > endpoint[maxPath+diag-1]:
+				// diagonal d+1 was farther along, so use that
+				aidx = endpoint[maxPath+diag+1] + 0
+			default:
+				// diagonal d-1 was farther (or the same), so use that
+				aidx = endpoint[maxPath+diag-1] + 1
+			}
+			// On diagonal d, we can compute bidx from aidx.
+			bidx := aidx - diag // y
+			// See how far we can go on this diagonal before we find a difference.
+			for aidx < alen && bidx < blen && a[aidx] == b[bidx] {
+				aidx++
+				bidx++
+			}
+			// Store the end of the current edit chain.
+			endpoint[maxPath+diag] = aidx
+			// If we've found the end of both inputs, we're done!
+			if aidx >= alen && bidx >= blen {
+				save() // save the final path
+				break dLoop
+			}
+		}
+		save() // save the current path
+	}
+	if editDistance == 0 {
+		return nil
+	}
+	chunks := make([]Chunk, editDistance+1)
+
+	x, y := alen, blen
+	for d := editDistance; d > 0; d-- {
+		endpoint := saved[d]
+		diag := x - y
+		insert := diag == -d || (diag != d && endpoint[maxPath+diag-1] < endpoint[maxPath+diag+1])
+
+		x1 := endpoint[maxPath+diag]
+		var x0, xM, kk int
+		if insert {
+			kk = diag + 1
+			x0 = endpoint[maxPath+kk]
+			xM = x0
+		} else {
+			kk = diag - 1
+			x0 = endpoint[maxPath+kk]
+			xM = x0 + 1
+		}
+		y0 := x0 - kk
+
+		var c Chunk
+		if insert {
+			c.Added = b[y0:][:1]
+		} else {
+			c.Deleted = a[x0:][:1]
+		}
+		if xM < x1 {
+			c.Equal = a[xM:][:x1-xM]
+		}
+
+		x, y = x0, y0
+		chunks[d] = c
+	}
+	if x > 0 {
+		chunks[0].Equal = a[:x]
+	}
+	if chunks[0].empty() {
+		chunks = chunks[1:]
+	}
+	if len(chunks) == 0 {
+		return nil
+	}
+	return chunks
 }
--- pkg/TFSMetrics/azure/client_test.go
+++ pkg/TFSMetrics/azure/client_test.go
@@ -37,17 +37,26 @@ func TestAzure_ChangedRows(t *testing.T) {
 	azure.TfvcClientConnection()
 
 	//ссылки на файлы
-	projectName := "Project2"
 	currentFileUrl := "$/Project2/test.txt"
-	previousFileUrl := "https://dev.azure.com/GnivcTestTaskTeam3/_apis/tfvc/items/$/Project2/test.txt?versionType=Changeset&version=17"
 
 	//получаем результат работы функции
-	addedRows, deletedRows, err := azure.ChangedRows(projectName, currentFileUrl, previousFileUrl)
+	addedRows, deletedRows, err := azure.ChangedRows(currentFileUrl)
 
 	//проверки
 	assert.NoError(t, err)
-	assert.Equal(t, 4, addedRows)
+	assert.Equal(t, 1, addedRows)
 	assert.Equal(t, 1, deletedRows)
+
+	//test one version
+	currentFileUrl = "$/Project2/testOneVersion"
+
+	//получаем результат работы функции
+	addedRows, deletedRows, err = azure.ChangedRows(currentFileUrl)
+
+	//проверки
+	assert.NoError(t, err)
+	assert.Equal(t, 41, addedRows)
+	assert.Equal(t, 0, deletedRows)
 }
 
 func TestGetItemVersions(t *testing.T) {
