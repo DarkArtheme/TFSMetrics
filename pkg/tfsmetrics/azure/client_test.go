4362f0bbf0a9b838db3fea98dbf4657cc4ef7f50
nazandr <nazadr72@gmail.com>
2021-09-28 21:24:47 +0300

    add ChangedRows test

--- pkg/tfsmetrics/azure/client_test.go
+++ pkg/tfsmetrics/azure/client_test.go
@@ -3,10 +3,10 @@ package azure
 import (
 	"errors"
 	"go-marathon-team-3/pkg/tfsmetrics/mock"
-
-	"time"
-
+	"io"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
@@ -31,8 +31,8 @@ func TestAzure_GetChangesetChanges(t *testing.T) {
 		Id:          1,
 		Author:      "Ivan",
 		Email:       "example@example.com",
-		AddedRows:   0,
-		DeletedRows: 0,
+		AddedRows:   2,
+		DeletedRows: 1,
 		Date:        time.Now(),
 		Message:     "hello world",
 		Hash:        "",
@@ -48,6 +48,33 @@ func TestAzure_GetChangesetChanges(t *testing.T) {
 			Comment:     &cs.Message,
 		}, nil)
 
+	version := "1"
+	currentFilePath := "currentFilePath"
+	mockedClient.
+		EXPECT().
+		GetChangesetChanges(azure.Config.Context, tfvc.GetChangesetChangesArgs{Id: &cs.Id}).
+		Return(&tfvc.GetChangesetChangesResponseValue{Value: []git.TfvcChange{
+			{Item: map[string]interface{}{"isFolder": true}},
+			{Item: map[string]interface{}{"path": currentFilePath, "version": version}},
+			{Item: map[string]interface{}{"path": "image.jpg", "version": version}},
+		}}, nil)
+
+	currentFileContent := io.ReadCloser(io.NopCloser(strings.NewReader("current file content\n row")))
+	previousFileContent := io.ReadCloser(io.NopCloser(strings.NewReader("previous file content")))
+
+	// две версии
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version}}).
+		Return(currentFileContent, nil)
+
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version, VersionOption: &git.TfvcVersionOptionValues.Previous}}).
+		Return(previousFileContent, nil)
+
 	changeSet, err := azure.GetChangesetChanges(&cs.Id, cs.ProjectName)
 	assert.NoError(t, err)
 	assert.Equal(t, &cs, changeSet)
@@ -62,4 +89,56 @@ func TestAzure_GetChangesetChanges(t *testing.T) {
 	changeSet, err = azure.GetChangesetChanges(&cs.Id, cs.ProjectName)
 	assert.Error(t, err)
 	assert.Nil(t, changeSet)
+}
+
+func TestAzure_ChangedRows(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockedClient := mock.NewMockClient(ctrl)
+
+	conf := NewConfig()
+	azure := Azure{
+		Config:     conf,
+		TfvcClient: mockedClient,
+	}
+	currentFilePath := "currentFilePath"
+	version := "1"
+	currentFileContent := io.ReadCloser(io.NopCloser(strings.NewReader("current file content\n row")))
+	previousFileContent := io.ReadCloser(io.NopCloser(strings.NewReader("previous file content")))
+
+	// две версии
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version}}).
+		Return(currentFileContent, nil)
+
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version, VersionOption: &git.TfvcVersionOptionValues.Previous}}).
+		Return(previousFileContent, nil)
+	addedRows, deletedRows, err := azure.ChangedRows(currentFilePath, version)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, addedRows)
+	assert.Equal(t, 1, deletedRows)
+
+	// новый файл одна версия
+	currentFileContent = io.ReadCloser(io.NopCloser(strings.NewReader("current file content\n row")))
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version}}).
+		Return(currentFileContent, nil)
+
+	mockedClient.
+		EXPECT().
+		GetItemContent(azure.Config.Context, tfvc.GetItemContentArgs{Path: &currentFilePath,
+			VersionDescriptor: &git.TfvcVersionDescriptor{Version: &version, VersionOption: &git.TfvcVersionOptionValues.Previous}}).
+		Return(nil, errors.New("error"))
+
+	addedRows, deletedRows, err = azure.ChangedRows(currentFilePath, version)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, addedRows)
+	assert.Equal(t, 0, deletedRows)
 }
