dbf4cb76550eb6d321c99def1d0bea2ac2515d88
nazandr <nazadr72@gmail.com>
2021-09-28 18:48:32 +0300

    realise exporter methods

--- pkg/tfsmetrics/exporter/exporter.go
+++ pkg/tfsmetrics/exporter/exporter.go
@@ -8,7 +8,7 @@ import (
 
 type Exporter interface {
 	GetDataByProject(iterator repointerface.CommitIterator, project string) map[string]ByProject
-	GetDataByAuthor(iterator repointerface.CommitIterator, author string) map[string]ByAuthor
+	GetDataByAuthor(iterator repointerface.CommitIterator, author string, project string) map[string]ByAuthor
 	// Принимает КОПИЮ итератора и создает по нему метрики для проекта
 	GetProjectMetrics(iterator repointerface.CommitIterator, project string)
 }
@@ -42,12 +42,14 @@ func newMetrics(author string, project string) *metrics {
 }
 
 type exporter struct {
-	authors map[string]*metrics
+	authors      map[string]*metrics
+	dataByAuthor map[string]ByAuthor
 }
 
 func NewExporter() Exporter {
 	return &exporter{
-		authors: make(map[string]*metrics),
+		authors:      make(map[string]*metrics),
+		dataByAuthor: make(map[string]ByAuthor),
 	}
 }
 
@@ -67,7 +69,7 @@ func (e *exporter) GetProjectMetrics(iterator repointerface.CommitIterator, proj
 }
 
 type ByAuthor struct {
-	Projects    string
+	Projects    []string
 	Commits     int
 	AddedRows   int
 	DeletedRows int
@@ -81,9 +83,48 @@ type ByProject struct {
 }
 
 func (e *exporter) GetDataByProject(iterator repointerface.CommitIterator, project string) map[string]ByProject {
-	return nil
+	res := make(map[string]ByProject)
+	for commit, err := iterator.Next(); err == nil; commit, err = iterator.Next() {
+		if proj, ok := res[project]; ok {
+			proj.Commits += 1
+			proj.AddedRows += commit.AddedRows
+			proj.DeletedRows += commit.DeletedRows
+		} else {
+			res[project] = ByProject{
+				Author:      commit.Author,
+				Commits:     1,
+				AddedRows:   commit.AddedRows,
+				DeletedRows: commit.DeletedRows,
+			}
+		}
+	}
+	return res
 }
 
-func (e *exporter) GetDataByAuthor(iterator repointerface.CommitIterator, author string) map[string]ByAuthor {
+func (e *exporter) GetDataByAuthor(iterator repointerface.CommitIterator, author string, project string) map[string]ByAuthor {
+	for commit, err := iterator.Next(); err == nil; commit, err = iterator.Next() {
+		if auth, ok := e.dataByAuthor[author]; ok {
+			is := false
+			for _, v := range auth.Projects {
+				if v == project {
+					is = true
+					break
+				}
+			}
+			if !is {
+				auth.Projects = append(auth.Projects, project)
+			}
+			auth.Commits += 1
+			auth.AddedRows += commit.AddedRows
+			auth.DeletedRows += commit.DeletedRows
+		} else {
+			e.dataByAuthor[author] = ByAuthor{
+				Projects:    []string{project},
+				Commits:     1,
+				AddedRows:   commit.AddedRows,
+				DeletedRows: commit.DeletedRows,
+			}
+		}
+	}
 	return nil
 }
