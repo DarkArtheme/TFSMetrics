16f7b69831327f4cd50ace750283226dc4646e24
DarkArtheme <artyom.bogdanov.2012@yandex.ru>
2021-10-02 18:21:39 +0300

    token deleted

--- pkg/tfsmetrics/metrics_test.go
+++ pkg/tfsmetrics/metrics_test.go
@@ -14,36 +14,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func Test_commitsCollection_Open(t *testing.T) {
-// 	conf := azure.NewConfig()
-// 	conf.OrganizationUrl = "https://dev.azure.com/GnivcTestTaskTeam3"
-// 	conf.Token = "yem42urypxdzuhceovddboakqs7skiicinze2i2u2leqrvbgblcq"
-
-// 	azure := azure.NewAzure(conf)
-// 	azure.Connect()
-
-// 	projects, err := azure.ListOfProjects()
-// 	require.NoError(t, err)
-
-// 	store, err := store.TestStore()
-// 	require.NoError(t, err)
-// 	defer store.Close()
-// 	defer func() {
-// 		os.Remove(store.DB.Path())
-// 	}()
-// 	for _, project := range projects {
-// 		commmits := NewCommitCollection(*project, azure, true, store)
-// 		err := commmits.Open()
-// 		require.NoError(t, err)
-// 		iter, err := commmits.GetCommitIterator()
-// 		require.NoError(t, err)
-
-// 		for commit, err := iter.Next(); err == nil; commit, err = iter.Next() {
-// 			fmt.Println(commit)
-// 		}
-// 	}
-
-// }
 
 func Test_iterator_Next_cahche_false(t *testing.T) {
 	ctrl := gomock.NewController(t)
